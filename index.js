ebc2b63a754cf0292a5adc892521f25dda9d7c90
danCode-s <dancodes9@gmail.com>
2024-09-28 14:48:40 +0500

    get the coordinates of the city with the api

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
+const API_Key = "";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-app.post("/city", (req, res)=>{
-    const city = req.body;
+
+app.post("/city", async (req, res)=>{
+    const city = req.body.city;
     res.render("index.ejs");
-    console.log(city);
+    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_Key}`);
+    const lat = response.data[0].lat;
+    const lon = response.data[0].lon;
+
+    
 })
 app.listen(port, ()=> {
     console.log("server running on port: " + port);
