972c579dffa3b44e0cb37afd8935ed4711a786df
danCode-s <dancodes9@gmail.com>
2024-09-28 15:06:14 +0500

    get the current weather and send it to index.ejs

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const app = express();
 const port = 3000;
 
 
-const API_Key = "";
+const API_Key = "94cd2ad3ce3a6c477292027ee79f5483";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs",);
 })
 
 
@@ -23,6 +23,8 @@ app.post("/city", async (req, res)=>{
     const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_Key}`);
     const lat = response.data[0].lat;
     const lon = response.data[0].lon;
+    const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,daily&appid=${API_Key}`);
+    res.render("index.ejs", {currentWeather: getWeather.data});
 
     
 })
