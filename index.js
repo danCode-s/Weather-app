1575cda09ba331816bf04969b4bcc0dfef0e7635
danCode-s <dancodes9@gmail.com>
2024-09-28 15:27:34 +0500

    show the data in the frontend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 
-const API_Key = "94cd2ad3ce3a6c477292027ee79f5483";
+const API_Key = "";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -19,11 +19,11 @@ app.get("/", (req, res) => {
 
 app.post("/city", async (req, res)=>{
     const city = req.body.city;
-    res.render("index.ejs");
     const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_Key}`);
     const lat = response.data[0].lat;
     const lon = response.data[0].lon;
     const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,daily&appid=${API_Key}`);
+    console.log(getWeather.data);
     res.render("index.ejs", {currentWeather: getWeather.data});
 
     
